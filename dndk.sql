37288e8f3c435a099d03422a6e2142b6cc732dbb
Nghiatran00 <nghiatran010401@gmail.com>
2023-04-26 01:00:19 +0700

    caro minimax

---
+++ dndk.sql
@@ -0,0 +1,18 @@
+﻿create database dndk
+GO
+USE dndk
+GO
+
+CREATE TABLE dndk
+(
+	tk nvarchar(50) Primary key,
+	mk nvarchar(50),
+)
+
+insert into  dn
+values('sa','123')
+go
+create proc kt
+on dn
+
+
